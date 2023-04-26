31a125030f5684dbf9b40a9069794af5195c7ce8
Mijail Potinski <128333142+mapkpo@users.noreply.github.com>
2023-04-26 18:18:05 -0300

    Add files via upload

---
+++ Cajon.hpp
@@ -0,0 +1,40 @@
+#include <string>
+using namespace std;
+
+class Cajon {
+private:
+    int id;
+    string tipo;
+    double cantidad;
+    double pesoMaximo;
+public:
+    Cajon(int id, string tipo, double pesoMaximo = 20.0) {
+        this->id = id;
+        this->tipo = tipo;
+        this->cantidad = 0.0;
+        this->pesoMaximo = pesoMaximo;
+    }
+    void agregarFruta(double cantidad) {
+        if (this->cantidad + cantidad > this->pesoMaximo) {
+            throw "El cajón está lleno";
+        }
+        this->cantidad += cantidad;
+    }
+    void quitarFruta(double cantidad) {
+        if (this->cantidad < cantidad) {
+            throw "No hay suficiente fruta en el cajón";
+        }
+        this->cantidad -= cantidad;
+    }
+    bool estaLleno() {
+        if (cantidad == pesoMaximo){
+            return true;
+        }
+    }
+    string getTipo() {
+        return this->tipo;
+    }
+    double getCantidad() {
+        return this->cantidad;
+    }
+};
